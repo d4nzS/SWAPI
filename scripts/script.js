87305f74c2e977b74aca193e3d15401a00c1ed9a
d4nzS <dzianis.karym@gmail.com>
2022-08-02 21:10:08 +0300

    initial script set up

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const SWAPI_URl = 'https://swapi.dev/api';
+const category = '/people';
+const mainURL = new URL(category, SWAPI_URl);
+
+const search = document.querySelector('.header__search');
+const cardList = document.querySelector('.main__list');
+const paginationList = document.querySelector('.footer__list');
