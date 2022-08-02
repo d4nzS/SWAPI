6cafca5cfcc8525720a39b3ac53bdaffe5b72b3e
d4nzS <dzianis.karym@gmail.com>
2022-08-02 21:25:33 +0300

    rewrite urls with url class

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const SWAPI_URl = 'https://swapi.dev/api';
+const SWAPI_URl = new URL('/api', 'https://swapi.dev');
 const category = '/people';
-const mainURL = new URL(category, SWAPI_URl);
+const mainURL = new URL(SWAPI_URl.pathname + category, SWAPI_URl);
 
 const search = document.querySelector('.header__search');
 const cardList = document.querySelector('.main__list');
